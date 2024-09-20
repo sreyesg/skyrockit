a050a37b62410a6b35c2136d39336b9fe34f6f48
sreyesg <santiagoreyes.g@gmail.com>
2024-09-19 20:41:44 -0700

    define embeded schema applicationSchema

--- models/user.js
+++ models/user.js
@@ -1,4 +1,29 @@
+const { application } = require('express');
 const mongoose = require('mongoose');
+
+const applicationSchema = new mongoose.Schema({
+  company: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  notes: {
+    type: String,
+    required: true,
+  },
+  postingLink: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: String,
+    required: true,
+    enum:['interested','applied','interviewing','rejected']},
+  
+  })
 
 const userSchema = mongoose.Schema({
   username: {
@@ -9,6 +34,7 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  applications: [applicationSchema],
 });
 
 const User = mongoose.model('User', userSchema);
