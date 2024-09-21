1692b734ddd1a6f14c673c92ad72d437639da373
sreyesg <santiagoreyes.g@gmail.com>
2024-09-21 09:33:42 -0700

    add nav bar and controllers logic to render it

--- controllers/applications.js
+++ controllers/applications.js
@@ -2,10 +2,17 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/user.js')
 
-router.get('/',(req, res) => {
-    res.send('hello applications index.')
+router.get('/',async (req, res) => {
+    try{
+        res.render('applications/index.ejs')
+    }catch(err){
+        console.log(err)
+        res.redirect('/')
+    }
 })
-
+router.get('/new', (req, res) => {
+    res.render('applications/new.ejs')
+})
 
 
 
